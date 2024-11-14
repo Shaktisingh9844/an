672fdcf5e38803cf59aae0a0d0e8fd2ae4c426f3
Shaktisingh9844 <shaktirathorehadiyal@gmail.com>
2024-11-14 13:44:29 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY SHARP
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7701709003:AAE0bT673x3UP9RWozuYLT5shVxdSmy-S5w")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@Vippaidhack7773")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("5424556385")
+
+
+
+
+
+
+
+  
